c017d4a4c63d2b228c3f400a3b5bbcf4751bb049
Kritarat.M <ideal141148@gmail.com>
2024-07-11 00:01:54 +0700

    add: message form

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ const menuTgglBtn = $("#menu-toggle-btn");
 $(menuTgglBtn).click(function (e) { 
     $(sideNav).toggleClass("w-[260px] w-0");
 });
+
+const messageChatgpt = $("#message-chatgpt");
+const sendMessageBtn = $("#send-message-btn");
+
+
+$(messageChatgpt).keydown(function (e) { 
+    if($(this).val().length < 2 && e.keyCode==8){
+        $(sendMessageBtn).removeClass("hover:bg-[#C6C6C6] bg-white");
+        $(sendMessageBtn).addClass("bg-[#676767]");
+    } else {
+        console.log("Chose 2");
+        $(sendMessageBtn).removeClass("bg-[#676767]");
+        $(sendMessageBtn).addClass("hover:bg-[#C6C6C6] bg-white");
+    }
+});
